8fe2d2f3f123f275080d7f6faad85e2d12b91f95
Rohi Modak <rohi@localhost>
2025-02-16 22:12:38 +0530

    Move some routes behind authentication

--- blog/router/blog.py
+++ blog/router/blog.py
@@ -8,23 +8,24 @@ router = APIRouter(prefix='/blog',tags=['Blogs'])
 get_db = database.get_db
 
 @router.post('', status_code=status.HTTP_201_CREATED)
-def create_blog(request: schemas.Blog, db: Session = Depends(get_db)):
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     new_blog = blog.create(request, db)
     return new_blog
 
 @router.get('', response_model=list[schemas.ShowBlog])
-def get_all(db: Session = Depends(get_db), get_current_user: schemas.User = Depends(oauth2.get_current_user)):
+def get_all(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    # get_current_user: schemas.User = Depends(oauth2.get_current_user)
     blogs = blog.get_all(db)
     return blogs
 
 @router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
-def show(id, db: Session = Depends(get_db)):
+def show(id, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.show(id, db)
 
 @router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def destroy(id, db: Session = Depends(get_db)):
+def destroy(id, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.delete(id, db)
 
 @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED)
-def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+def update(id, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
     return blog.update(id, request, db)
